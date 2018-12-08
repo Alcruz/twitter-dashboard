342e511688df37d43851e9371f7290b5b07ece4f
Alvin De la Cruz <alvinj.delacruz@gmail.com>
2018-12-08 12:10:40 -0400

    Render tweet content and information

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const RT_PATTERN = /RT @\w+:\ */;
+
 var twitterApi = {
     baseUrl: 'http://localhost:7890/1.1/',
     getLastestTweet(user, count = 30) {
@@ -19,5 +21,48 @@ var app =  new Vue({
         .then(results => this.usersTweets = results);
     }
 });
+
+app.getRetweetUser = text => {
+    return text.match(RT_PATTERN)[0].replace('@', '').replace('RT', '').replace(':', '').trim();
+}
+app.getUserMentionName = tweet => {
+    const username = app.getRetweetUser(tweet.text);
+    return tweet.entities
+        .user_mentions
+        .filter(um => um.screen_name === username)[0].name;
+}
+
+app.getElapsedTime = (dateStr) => {
+    const date = new Date(dateStr);
+    let elapsedMiliseconds = (new Date() - date) / 1000;
+
+    if (elapsedMiliseconds < 60)
+        return 'Now';
+
+    elapsedMiliseconds = elapsedMiliseconds / 60;
+    if (elapsedMiliseconds < 60) 
+        return `${Math.floor(elapsedMiliseconds)}m`;
+    
+    elapsedMiliseconds = elapsedMiliseconds / 60;
+    if (elapsedMiliseconds < 24) 
+        return `${Math.floor(elapsedMiliseconds)}h`;
+    
+    elapsedMiliseconds = elapsedMiliseconds / 24;
+    if (elapsedMiliseconds < 7) 
+        return `${Math.floor(elapsedMiliseconds)}d`;
+
+    elapsedMiliseconds = elapsedMiliseconds / 7;
+    if (elapsedMiliseconds < 4) 
+        return `${Math.floor(elapsedMiliseconds)}w`;
+
+    elapsedMiliseconds = elapsedMiliseconds / 4;
+    if (elapsedMiliseconds < 12)  
+        return date.toLocaleString("en-us", {
+            month: "long"
+          }).slice(9, 3);    
+
+    elapsedMiliseconds = elapsedMiliseconds / 12;
+    return `${Math.floor(elapsedMiliseconds)}y`;    
+}
 
 app.$mount('#app')
